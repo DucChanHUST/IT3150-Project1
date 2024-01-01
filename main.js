ee3ccef8ad6b4bfb22fa343fa7251e48832effd7
DucChanHUST <kuntakufcvn@gmail.com>
2024-01-02 02:11:33 +0700

    need to upgrade

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 // main.js
 const { readFile } = require("./readFile");
-const { generateDistanceMatrix } = require("./distanceMatrix")
+const { calculateCost } = require("./calculateCost");
 const { generateSavingList } = require("./savingList");
+const { generateDistanceMatrix } = require("./distanceMatrix");
 
 const filePath = "./dataset/A-n32-k5.vrp";
+
+const checkExist = (route, i, j) => {
+  for (let k = 0; k < route.length; k++) {
+    if (
+      route[k].slice(1, -1).includes(i) ||
+      route[k].slice(1, -1).includes(j) ||
+      (route[k].includes(i) && route[k].includes(j))
+    ) {
+      return true;
+    }
+  }
+  return false;
+};
 
 const CVRP = async () => {
   const { CAPACITY, trucks, DIMENSION, NODE_COORD_SECTION, DEMAND_SECTION } =
@@ -13,8 +27,188 @@ const CVRP = async () => {
 
   const savingList = generateSavingList(distanceMatrix);
 
-  console.log(savingList);
+  let capacity = Array(trucks).fill(CAPACITY);
+  let route = [];
+  for (let i = 0; i < trucks; i++) {
+    route.push([]);
+  }
+  let usedRoute = 0;
+  // usedRoute < trucks
 
+  savingList.map((item, index) => {
+    // merge
+    for (let k = 0; k < usedRoute - 1; k++) {
+      for (let l = k + 1; l < usedRoute; l++) {
+        if (
+          (route[k][0] === item.i && route[l][0] === item.j) ||
+          (route[k][0] === item.j && route[l][0] === item.i)
+        ) {
+          if (capacity[k] >= CAPACITY - capacity[l]) {
+            route[k] = route[k].concat(route[l]);
+            capacity[k] -= CAPACITY - capacity[l];
+            route.splice(l, 1);
+            capacity.splice(l, 1);
+            route.push([]);
+            capacity.push(CAPACITY);
+            usedRoute--;
+            console.log(route, index, "m1");
+            console.log(capacity);
+            return;
+          } else {
+            return;
+          }
+        }
+
+        if (
+          (route[k][route[k].length - 1] === item.i &&
+            route[l][route[l].length - 1] === item.j) ||
+          (route[k][route[k].length - 1] === item.j &&
+            route[l][route[l].length - 1] === item.i)
+        ) {
+          if (capacity[k] >= CAPACITY - capacity[l]) {
+            route[k] = route[k].concat(route[l]);
+            capacity[k] -= CAPACITY - capacity[l];
+            route.splice(l, 1);
+            capacity.splice(l, 1);
+            route.push([]);
+            capacity.push(CAPACITY);
+            usedRoute--;
+            console.log(route, index, "m2");
+            console.log(capacity);
+            return;
+          } else {
+            return;
+          }
+        }
+
+        if (
+          (route[k][0] === item.i &&
+            route[l][route[l].length - 1] === item.j) ||
+          (route[k][0] === item.j && route[l][route[l].length - 1] === item.i)
+        ) {
+          if (capacity[k] >= CAPACITY - capacity[l]) {
+            route[k] = route[k].concat(route[l].reverse());
+            capacity[k] -= CAPACITY - capacity[l];
+            route.splice(l, 1);
+            capacity.splice(l, 1);
+            route.push([]);
+            capacity.push(CAPACITY);
+            usedRoute--;
+            console.log(route, index, "m3");
+            console.log(capacity);
+            return;
+          } else {
+            return;
+          }
+        }
+
+        if (
+          (route[k][route[k].length - 1] === item.i &&
+            route[l][0] === item.j) ||
+          (route[k][route[k].length - 1] === item.j && route[l][0] === item.i)
+        ) {
+          if (capacity[k] >= CAPACITY - capacity[l]) {
+            route[k] = route[k].concat(route[l].reverse());
+            capacity[k] -= CAPACITY - capacity[l];
+            route.splice(l, 1);
+            capacity.splice(l, 1);
+            route.push([]);
+            capacity.push(CAPACITY);
+            usedRoute--;
+            console.log(route, index, "m4");
+            console.log(capacity);
+            return;
+          } else {
+            return;
+          }
+        }
+      }
+    }
+    // a. neither i nor j have already been assigned to a route,
+    // in which case a new route is initiated including both i and j.
+
+    if (usedRoute < trucks) {
+      let check = true;
+      for (let k = 0; k < usedRoute; k++) {
+        if (route[k].includes(item.i) || route[k].includes(item.j)) {
+          check = false;
+          break;
+        }
+      }
+
+      if (check) {
+        route[usedRoute].push(item.i);
+        route[usedRoute].push(item.j);
+        capacity[usedRoute] -= DEMAND_SECTION[item.i].demand;
+        capacity[usedRoute] -= DEMAND_SECTION[item.j].demand;
+        usedRoute++;
+        console.log(route, index, "*");
+        console.log(capacity);
+        return;
+      }
+    }
+
+    // b. exactly one of the two points (i or j) has already been included
+    // in an existing route and that point is not interior to that route
+    // in which case the link (i, j) is added to that same route.
+
+    for (let k = 0; k < usedRoute; k++) {
+      if (checkExist(route, item.i, item.j)) {
+        return;
+      }
+
+      if (
+        route[k][0] === item.i &&
+        capacity[k] >= DEMAND_SECTION[item.j].demand
+      ) {
+        route[k].unshift(item.j);
+        capacity[k] -= DEMAND_SECTION[item.j].demand;
+        console.log(route, index, "a");
+        console.log(capacity);
+        return;
+      }
+
+      if (
+        route[k][0] === item.j &&
+        capacity[k] >= DEMAND_SECTION[item.i].demand
+      ) {
+        route[k].unshift(item.i);
+        capacity[k] -= DEMAND_SECTION[item.i].demand;
+        console.log(route, index, "b");
+        console.log(capacity);
+        return;
+      }
+
+      if (
+        route[k][route[k].length - 1] === item.i &&
+        capacity[k] >= DEMAND_SECTION[item.j].demand
+      ) {
+        route[k].push(item.j);
+        capacity[k] -= DEMAND_SECTION[item.j].demand;
+        console.log(route, index, "c");
+        console.log(capacity);
+        return;
+      }
+
+      if (
+        route[k][route[k].length - 1] === item.j &&
+        capacity[k] >= DEMAND_SECTION[item.i].demand
+      ) {
+        route[k].push(item.i);
+        capacity[k] -= DEMAND_SECTION[item.i].demand;
+        console.log(route, index, "d");
+        console.log(capacity);
+        return;
+      }
+    }
+  });
+
+  const cost = calculateCost(distanceMatrix, route);
+
+  console.log(cost);
+  console.log(route);
+  console.log(capacity);
+  console.log();
 };
 
 CVRP();
--- readme.md
+++ readme.md
@@ -1,2 +1,5 @@
 #dataset
 http://vrp.galgos.inf.puc-rio.br/index.php/en/
+
+#documentation
+https://web.mit.edu/urban_or_book/www/book/chapter6/6.4.12.html
