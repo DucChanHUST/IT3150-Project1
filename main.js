bb7be5ed08a7688917514510d7b00e434a4d665c
DucChanHUST <kuntakufcvn@gmail.com>
2024-01-22 18:53:55 +0700

    clarke and wright savings algorithm

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const { readFile } = require("./readFile");
 const { writeFile } = require("./writeFile");
 const { calculateCost } = require("./calculateCost");
 const { generateSavingList } = require("./savingList");
-const { generateSavingListGA } = require("./savingListGA");
 const { generateDistanceMatrix } = require("./distanceMatrix");
 
 const filePath = "./dataset/A-n32-k5.vrp";
@@ -20,29 +19,6 @@ const CVRP = async () => {
   savingList = generateSavingList(distanceMatrix);
   route = solution(savingList, trucks, CAPACITY, DEMAND_SECTION);
   cost = calculateCost(distanceMatrix, route);
-
-  const iteration = savingList.length * 1000;
-
-  for (let index = 0; index < iteration; index++) {
-    const newSavingList = generateSavingListGA(savingList);
-    if (newSavingList.length) {
-      const newRoute = solution(
-        newSavingList,
-        trucks,
-        CAPACITY,
-        DEMAND_SECTION
-      );
-      try {
-        const newCost = calculateCost(distanceMatrix, newRoute);
-        if (newCost < cost) {
-          route = newRoute;
-          cost = newCost;
-          savingList = newSavingList;
-          console.log(index, cost);
-        }
-      } catch (error) {}
-    }
-  }
 
   console.log(cost);
   writeFile(NODE_COORD_SECTION, route, cost);
